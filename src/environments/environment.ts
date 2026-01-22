c15eec2de2de9637537033f15d330f0107e0384e
Melek Ajroud <melek.ajroud@gmail.com>
2026-01-22 14:42:43 +0100

    cleanup: removed recursive backend folders

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: false,
   OLLAMA_API_URL: '/api/chat',
-  PHP_API_URL: '/api/php'
+  PHP_API_URL: '/api/php',
+  api1Url: 'https://mon-compte.rafi9ni.pro/mobile/AI/SearchHomeWork/Rafi9ni',
+  api2Url: 'https://mon-compte.rafi9ni.pro/mobile/homeWork/getDetail'
 };
+
+
